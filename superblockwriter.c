759fe028cf1854fefb3d48474ba25f6c17bf052f
Yash M Sawant <yashmrsawant@localhost.localdomain>
2017-06-10 14:27:37 +0530

    Edited authors name

--- superblockwriter.c
+++ superblockwriter.c
@@ -4,5 +4,25 @@
  *  Created on: 10-Jun-2017
  *      Author: yashmrsawant
  */
+/**
+ *  Authors : Yash M Sawant
+ *  		  Abhilash Kurmi
+ *
+ */
 
+int writer() {
+	char filename[4];
+	int i, j;
 
+	FILE* fssimulator;
+
+	struct superblock sb;
+
+	LLT dataWritingStart;
+	LLT dataWritingArea;
+
+	LLT dataBlocksCover;
+	LLT dataBlocks;
+	LLT dataBlocksPointer;
+
+}
